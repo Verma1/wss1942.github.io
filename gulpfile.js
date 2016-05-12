8f4a898399bcc015022500509e8634fec1ea6484
wss <wangshushuo@qq.com>
2016-05-12 20:26:18 +0800

    怎么在gulp中使用mustache还没整明白

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
 var gulp = require('gulp');
+var mustache = require('mustache');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
+gulp.task('default', function() {
+	mustache.render(params.json, me.tpl, output.html);
+});
+
 // 监视文件改动并重新载入
 gulp.task('serve', function() {
-  browserSync({
-    server: {
-      baseDir: './'
-    }
-  });
+    browserSync({
+        server: {
+            baseDir: './'
+        }
+    });
 
-  gulp.watch(['*.html', 'stylesheets/*.css', 'js/*.js']).on('change', reload);
-
-
+    gulp.watch(['*.html', 'stylesheets/*.css', 'js/*.js'], ['default'])
+        .on('change', reload);
 });

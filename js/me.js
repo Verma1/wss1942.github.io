f3ddc9aa6ac31d7076384007e4be1db1e3a4898c
wss <wangshushuo@qq.com>
2016-05-16 17:34:46 +0800

    学习JQuery

---
+++ js/me.js
@@ -0,0 +1,37 @@
+var $ = function(selector, context) {
+    return new $.fn.init(selector, context);
+}
+
+$.fn = $.prototype;
+
+$.fn.init = function(selector, context) {
+    var nodelist = (context || document).querySelectorAll(selector),
+        i;
+    this.length = nodelist.length;
+    for (i = 0; i < this.length; i += 1) {
+        this[i] = nodelist[i];
+    }
+
+    return this;
+};
+
+$.fn.hide = function() {
+    this.each(function() {
+        this.style.display = 'none';
+    });
+
+    return this;
+}
+
+$.fn.each = function(fn) {
+    var length = this.length,
+        i;
+
+    for (i = 0; i < length; i += 1) {
+        fn.call(this[i], i, this[i]);
+    }
+}
+
+
+
+$('#n').hide();
